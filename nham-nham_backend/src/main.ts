a546061e874adadfeabdb015e079d21f9b709e32
Arifin Depay Jr <121757305+Arifindepayjr1@users.noreply.github.com>
2026-05-04 19:33:26 +0700

    feat: add new not-found html file

--- nham-nham_backend/src/main.ts
+++ nham-nham_backend/src/main.ts
@@ -1,21 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NextFunction, Request, Response } from 'express';
-import { HttpStatus, NotFoundException } from '@nestjs/common';
-import { readFileSync } from 'fs';
-import { join } from 'path';
-
-const notfoundHtml = readFileSync(
-    join(__dirname, "..", "public", "notfound.html"),
-    'utf-8'
-);
+import { AllExceptionFilter } from './common/filters/all-exception.filter';
+import { NotFoundExceptionFilter } from './common/filters/not-found-exception.filter';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-
+    app.useGlobalFilters(
+        new AllExceptionFilter(),
+        new NotFoundExceptionFilter(),
+    );
     await app.listen(process.env.PORT ?? 3000);
-
-
     console.log(`Running On http://localhost:${process.env.PORT}`);
 }
 bootstrap();
